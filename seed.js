8ec1b7970d785645f8147a3f4c35e4d60c01b11a
fridtjof hovik <fridtjofgh@gmail.com>
2016-07-14 15:01:31 -0700

    add autors to base

--- seed.js
+++ seed.js
@@ -54,6 +54,42 @@ var books_list = [
   }
 ];
 
+
+var authors_list = [
+  {
+    name: "Harper Lee",
+    alive: false
+  },
+  {
+    name: "F Scott Fitzgerald",
+    alive: false
+  },
+  {
+    name: "Victor Hugo",
+    alive: false
+  },
+  {
+    name: "Jules Verne",
+    alive: false
+  },
+  {
+    name: "Sheryl Sandberg",
+    alive: true
+  },
+  {
+    name: "Tim Ferriss",
+    alive: true
+  },
+  {
+    name: "John Steinbeck",
+    alive: false
+  },
+  {
+    name: "William Shakespeare",
+    alive: false
+  }
+];
+
 db.Book.remove({}, function(err, books){
 
   db.Book.create(books_list, function(err, books){
